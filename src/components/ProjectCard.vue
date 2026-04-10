0e0ba3151aa7c755ee82a592030c69af8a7be6cb
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-04-10 15:28:37 -0300

    fix: Arrumando link de import de assets

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <iframe src="{{ video_url }}" width="600" height="400" title="{{ title }}"></iframe>
+        <div>
+            <h3>{{ project_name }}</h3>
+            <p>{{ description }}</p>
+            <span class="text-secondary">Ver mais</span>
+        </div>
+    </div>
+</template>
---
+++ src/components/Projects.vue
@@ -0,0 +1,10 @@
+<template>
+    <section id="projetos">
+        <div class="section-container my-12">
+            <h2 class="subtitle text-center my-12">Meus Projetos</h2>
+            <div class="flex gap-5">
+
+            </div>
+        </div>
+    </section>
+</template>
